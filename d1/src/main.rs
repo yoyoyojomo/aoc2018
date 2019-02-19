8b17cf6eee8c48816601abebcbd87ff2627731e0
Joe Mou <joe@mou.fo>
2019-02-18 23:49:54 -0500

    day 1 paired w/ Colin

---
+++ d1/src/main.rs
@@ -0,0 +1,9 @@
+fn try_parse(s: String) -> i32 {
+    unimplemented!();
+}
+
+fn main() {
+    let input = include_str!("../input");
+    let sum: i32 = input.lines().map(|x| x.parse::<i32>().unwrap()).sum();
+    println!("{}", sum);
+}
