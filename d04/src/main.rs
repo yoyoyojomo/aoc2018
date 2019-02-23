33c4473cd9c0ceeb9f36b9315b41207d7939ac15
Joe Mou <joe@mou.fo>
2019-02-23 10:06:35 -0500

    day 4. proper error handling

--- d04/src/main.rs
+++ d04/src/main.rs
@@ -1,31 +1,32 @@
+use std::error::Error;
+use std::collections::HashMap;
 use std::io::{self, BufRead};
 use std::iter::Peekable;
 use std::str::{Chars, FromStr};
 
-type Result<T> = ::std::result::Result<T, Box<::std::error::Error>>;
+type Result<T> = ::std::result::Result<T, Box<Error>>;
 
 // TODO try as a newtype?
 struct StrParser<'a> {
-    iter: Peekable<Chars<'a>>,
+    it: Peekable<Chars<'a>>,
 }
 
 impl<'a> StrParser<'a> {
     fn new(s: &'a str) -> Self {
-        Self { iter: s.chars().peekable() }
-    }
-
-    fn done(&mut self) -> bool {
-        // TODO more succinct way to write this?
-        match self.iter.peek() {
-            Some(_) => false,
-            None => true,
+        Self {
+            it: s.chars().peekable(),
         }
     }
 
-    // TODO for this to work predictably, nothing should be consumed on error
+    fn done(&mut self) -> bool {
+        self.it.peek().is_none()
+    }
+
     fn consume_str(&mut self, s: &str) -> Result<()> {
+        let orig_iter = self.it.clone();
         for c in s.chars() {
-            if self.iter.next() != Some(c) {
+            if self.it.next() != Some(c) {
+                self.it = orig_iter;
                 return Err("unexpected parse".into());
             }
         }
@@ -34,12 +35,12 @@ impl<'a> StrParser<'a> {
 
     fn parse_usize(&mut self) -> Result<usize> {
         let mut digits = String::new();
-        while let Some(&c) = self.iter.peek() {
+        while let Some(&c) = self.it.peek() {
             if !c.is_numeric() {
                 break;
             }
             digits.push(c);
-            self.iter.next();
+            self.it.next();
         }
         Ok(digits.parse()?)
     }
@@ -63,7 +64,7 @@ struct Event {
 }
 
 impl FromStr for Event {
-    type Err = Box<::std::error::Error>;
+    type Err = Box<Error>;
 
     fn from_str(s: &str) -> Result<Event> {
         let mut parser = StrParser::new(s);
@@ -83,11 +84,9 @@ impl FromStr for Event {
             let guard = parser.parse_usize()?;
             parser.consume_str(" begins shift")?;
             Action::BeginShift { guard }
-        // TODO weird "f" truncation because of consume_str issue
-        } else if let Ok(()) = parser.consume_str("alls asleep") {
+        } else if let Ok(()) = parser.consume_str("falls asleep") {
             Action::Sleep
-        // TODO ditto "wak"
-        } else if let Ok(()) = parser.consume_str("es up") {
+        } else if let Ok(()) = parser.consume_str("wakes up") {
             Action::Wake
         } else {
             return Err("unexpected action".into());
@@ -96,15 +95,100 @@ impl FromStr for Event {
             return Err("trailing input".into());
         }
         Ok(Event {
-            year, month, day, hour, min, action,
+            year,
+            month,
+            day,
+            hour,
+            min,
+            action,
         })
     }
 }
 
+enum GuardState {
+    Initial,
+    Awake { guard: usize },
+    Asleep { guard: usize, asleep_min: usize },
+}
+
 fn main() -> Result<()> {
-    for line in io::stdin().lock().lines() {
-        let event: Event = line?.parse()?;
-        dbg!(event);
+    let mut sleep_by_guard = HashMap::new();
+    let mut state = GuardState::Initial;
+    let mut lines = io::stdin()
+        .lock()
+        .lines()
+        .collect::<std::result::Result<Vec<_>, _>>()?;
+    lines.sort();
+    for line in lines {
+        let event: Event = line.parse()?;
+        match event.action {
+            Action::BeginShift { guard } => match state {
+                GuardState::Initial | GuardState::Awake { .. } => {
+                    state = GuardState::Awake { guard };
+                }
+                GuardState::Asleep { .. } => return Err("guard change while asleep".into()),
+            },
+            Action::Sleep => match state {
+                GuardState::Awake { guard } => {
+                    state = GuardState::Asleep {
+                        guard,
+                        asleep_min: event.min,
+                    };
+                }
+                _ => return Err("no awake guard to sleep".into()),
+            },
+            Action::Wake => match state {
+                GuardState::Asleep { guard, asleep_min } => {
+                    let awake_min = event.min;
+                    if awake_min < asleep_min {
+                        return Err("out of order events".into());
+                    }
+                    let mins = sleep_by_guard
+                        .entry(guard)
+                        .or_insert_with(|| vec![0u32; 60]);
+                    for i in asleep_min..awake_min {
+                        mins[i] += 1;
+                    }
+                    state = GuardState::Awake { guard };
+                }
+                _ => return Err("no asleep guard to wake".into()),
+            },
+        }
     }
+
+    println!("{}", part1(&sleep_by_guard)?);
+    println!("{}", part2(&sleep_by_guard)?);
     Ok(())
+}
+
+fn part1(sleep_by_guard: &HashMap<usize, Vec<u32>>) -> Result<usize> {
+    let (_, guard) = sleep_by_guard
+        .iter()
+        .map(|(guard, sleep)| (sleep.iter().sum::<u32>(), guard))
+        .max()
+        .ok_or_else(|| Box::<Error>::from("empty input"))?;
+    let (_, min) = sleep_by_guard[guard]
+        .iter()
+        .enumerate()
+        .map(|(min, count)| (count, min))
+        .max()
+        .expect("no sleep");
+    Ok(guard * min)
+}
+
+fn part2(sleep_by_guard: &HashMap<usize, Vec<u32>>) -> Result<usize> {
+    let (_, guard, min) = sleep_by_guard
+        .iter()
+        .map(|(guard, sleep)| {
+            let (count, min) = sleep
+                .iter()
+                .enumerate()
+                .map(|(min, count)| (count, min))
+                .max()
+                .expect("no sleep");
+            (count, guard, min)
+        })
+        .max()
+        .ok_or_else(|| Box::<Error>::from("empty input"))?;
+    Ok(guard * min)
 }
