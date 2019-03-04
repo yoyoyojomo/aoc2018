42096a4119413e9c1d54c02b285711431fe74233
Joe Mou <joe@mou.fo>
2019-03-04 02:21:10 -0500

    day 12 part 2

--- d12/src/main.rs
+++ d12/src/main.rs
@@ -1,11 +1,58 @@
 use std::collections::BTreeMap;
 use std::error::Error;
 use std::io::{self, BufRead};
+use std::iter;
 use std::result;
 
 type Result<T> = result::Result<T, Box<Error>>;
 
-const PADDING: usize = 100;
+struct Pots {
+    state: Vec<u8>,
+    transitions: BTreeMap<Vec<u8>, u8>,
+    offset: usize,
+}
+
+impl Pots {
+    fn new(mut state: Vec<u8>, transitions: BTreeMap<Vec<u8>, u8>) -> Self {
+        state.splice(0..0, iter::repeat(b'.').take(32));
+        Self { state, transitions, offset: 32 }
+    }
+
+    fn maybe_grow(&mut self) {
+        if self.state[2] == b'#' {
+            self.state.splice(0..0, iter::repeat(b'.').take(self.offset));
+            self.offset *= 2;
+        }
+        if self.state[self.state.len() - 3] == b'#' {
+            self.state.extend(iter::repeat(b'.').take(self.offset));
+        }
+    }
+
+    fn evolve(&mut self) -> Result<()> {
+        self.maybe_grow();
+        let mut next = vec![b'.'; self.state.len()];
+        for i in 2..self.state.len() - 2 {
+            next[i] = *self.transitions
+                .get(&self.state[i - 2..i + 3])
+                .ok_or("no transition found")?;
+        }
+        self.state = next;
+        Ok(())
+    }
+
+    fn trimmed(&self) -> Result<Vec<u8>> {
+        Ok(String::from_utf8(self.state.clone())?.trim_matches('.').to_owned().into_bytes())
+    }
+
+    fn sum(&self) -> i64 {
+        self.state
+            .iter()
+            .enumerate()
+            .filter(|&(_, &c)| c == b'#')
+            .map(|(i, _)| (i as i64) - (self.offset as i64))
+            .sum()
+    }
+}
 
 fn parse_transition(mut line: Vec<u8>) -> Result<(Vec<u8>, u8)> {
     if line.len() != 10 || &line[5..9] != b" => " {
@@ -14,19 +61,6 @@ fn parse_transition(mut line: Vec<u8>) -> Result<(Vec<u8>, u8)> {
     let to = line[9];
     line.truncate(5);
     Ok((line, to))
-}
-
-fn evolve(state: &[u8], transitions: &BTreeMap<Vec<u8>, u8>) -> Result<Vec<u8>> {
-    let mut result = vec![b'.'; state.len()];
-    for i in 2..state.len() - 2 {
-        result[i] = *transitions
-            .get(&state[i - 2..i + 3])
-            .ok_or("no transition found")?;
-    }
-    if result[2] == b'#' || result[result.len() - 3] == b'#' {
-        return Err("insufficient padding".into());
-    }
-    Ok(result)
 }
 
 fn main() -> Result<()> {
@@ -43,19 +77,26 @@ fn main() -> Result<()> {
         .map(|l| parse_transition(l?))
         .collect::<result::Result<BTreeMap<_, _>, _>>()?;
 
-    let mut state = vec![b'.'; 2 * PADDING + initial.len()];
-    state[PADDING..PADDING + initial.len()].copy_from_slice(&initial);
+    let mut pots = Pots::new(initial, transitions);
     for _ in 0..20 {
-        state = evolve(&state, &transitions)?;
+        pots.evolve()?;
     }
+    println!("{}", pots.sum());
 
-    let sum: i32 = state
-        .iter()
-        .enumerate()
-        .filter(|&(_, &c)| c == b'#')
-        .map(|(i, _)| (i as i32) - (PADDING as i32))
-        .sum();
-    println!("{}", sum);
+    let mut last_state = pots.trimmed()?;
+    for i in 20..50000000000_u64 {
+        pots.evolve()?;
+        let state = pots.trimmed()?;
+        if state == last_state {
+            let sum = pots.sum();
+            pots.evolve()?;
+            let delta = pots.sum() - sum;
+            let projection = sum + delta * (50000000000_u64 - i - 1) as i64;
+            println!("{}", projection);
+            break;
+        }
+        last_state = state;
+    }
 
     Ok(())
 }
